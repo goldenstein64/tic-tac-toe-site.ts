fdd8e5085d5e7f2613abc3d9f24427e1f5e45715
goldenstein64 <goldnstein64@gmail.com>
2024-08-29 03:44:53 -0400

    feat: db ddl uses strict tables

--- game-ddl.sql
+++ game-ddl.sql
@@ -1,18 +1,18 @@
 CREATE TABLE User (
 	id INT PRIMARY KEY,
-	username NVARCHAR(32) CHECK (LENGTH(username) BETWEEN 8 AND 32),
+	username TEXT CHECK (LENGTH(username) BETWEEN 8 AND 32),
 	winCount INT DEFAULT (0)
-);
+) STRICT;
 
 CREATE TABLE IsComputer (
 	userId INT PRIMARY KEY REFERENCES User(id)
-);
+) STRICT;
 
 CREATE TABLE Game (
 	id INT PRIMARY KEY,
 	playerX INT REFERENCES User(id),
 	playerO INT REFERENCES User(id)
-);
+) STRICT;
 
 CREATE TABLE Move (
 	gameId INT REFERENCES Game(id),
@@ -20,7 +20,7 @@ CREATE TABLE Move (
 	position INT, -- 1-9, left-to-right, top-to-bottom
 	-- the player is inferred from ordering
 	PRIMARY KEY (gameId, ordering)
-);
+) STRICT;
 
 INSERT INTO User (id, username, winCount) VALUES
 	(1, 'EasyComputer', 0),
