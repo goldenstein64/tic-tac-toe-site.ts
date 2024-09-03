55a150a9c878897f6c7f0e412fcee5982a16a8ec
goldenstein64 <goldnstein64@gmail.com>
2024-09-03 15:33:31 -0400

    feat: wip create game ui

--- src/routes/game-list-api.ts
+++ src/routes/game-list-api.ts
@@ -1,3 +1,25 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 
-export default new Elysia();
+import { db } from "../libs/db";
+
+const insertGame = db.query<{}, []>(`
+  INSERT INTO Game (playerX, playerO) VALUES ($playerX, $playerO)
+`);
+
+const playerType = t.Union([
+  t.Literal("human"),
+  t.Literal("easyComputer"),
+  t.Literal("mediumComputer"),
+  t.Literal("hardComputer"),
+]);
+
+export default new Elysia({ prefix: "/api" })
+  .post("/new-game", ({ body, set }) => {}, {
+    body: t.Object({
+      typeX: playerType,
+      typeO: playerType,
+    }),
+    type: "application/x-www-form-urlencoded",
+  })
+  .get("/active-games", (ctx) => {}, {})
+  .get("/available-games", (ctx) => {}, {});
