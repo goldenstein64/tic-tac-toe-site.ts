84bac65ad2f67bfaa87a342e7ec9339a6e8a9674
goldenstein64 <goldnstein64@gmail.com>
2024-10-20 14:22:41 -0400

    feat: username modal

---
+++ src/components/username-modal.tsx
@@ -0,0 +1,34 @@
+import { Html } from "@elysiajs/html";
+
+export function UsernameHead() {
+  return (
+    <>
+      <link rel="stylesheet" href="/public/modal.css" />
+      <script defer type="module" src="/public/username-modal.js" />
+    </>
+  );
+}
+
+export function UsernameModal() {
+  return (
+    <dialog id="username-modal">
+      <form hx-put="/api/username" hx-swap="none">
+        <h2>Enter your username</h2>
+        <input
+          type="text"
+          id="username-input"
+          name="username"
+          autofocus="true"
+          placeholder="Username"
+        />
+        <ul>
+          <li>Must be between 1 and 32 characters</li>
+          <li>Must be entirely alphanumeric</li>
+        </ul>
+        <button id="username-submit" type="submit" hx-disabled-elt="this">
+          Submit
+        </button>
+      </form>
+    </dialog>
+  );
+}
