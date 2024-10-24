b65c6c79912155f10e719971aa280c7952d9ce5e
goldenstein64 <goldnstein64@gmail.com>
2024-10-24 01:27:03 -0400

    feat: /api/username

--- src/routes/user-api.tsx
+++ src/routes/user-api.tsx
@@ -1,58 +1,186 @@
 import { Html, html } from "@elysiajs/html";
 import { Elysia, t } from "elysia";
-import { eq, sql } from "drizzle-orm";
+import { and, eq, gt, sql, notExists, or, exists, lte } from "drizzle-orm";
 
-import { User } from "../db/schema";
+import { ActiveLobby, FinishedLobby, Game, Lobby, User } from "../db/schema";
 import { db } from "../db";
+import { SQLiteColumn } from "drizzle-orm/sqlite-core";
+import { intString } from "../types";
 
-const insertUser = db
-  .insert(User)
-  .values({
-    username: sql.placeholder("username"),
-  })
-  .prepare();
+const ALPHANUMERIC = /\w+/;
+const DAY_LENGTH = 86_400;
+const UNIX_EPOCH = sql<number>`(unixepoch())`;
 
-const selectUser = db
-  .select({ id: User.id, username: User.username })
-  .from(User)
-  .where(eq(User.username, sql.placeholder("username")))
-  .prepare();
+const USER_AGE = sql<number>`${UNIX_EPOCH} - ${User.createdAt}`;
 
-export default new Elysia({ prefix: "/api" }).use(html()).post(
-  "/user",
-  async ({ body: { username }, cookie: { userId: userIdCookie } }) => {
-    const users = await selectUser.execute({ username });
-    userIdCookie.maxAge = 86400;
-    userIdCookie.sameSite = "strict";
-    if (users.length > 0) {
-      // a user exists with this username, tell them they can't use it
-      return (
-        <div>
-          <input
-            hx-select="#set-username"
-            hx-swap="outerHTML"
-            id="set-username"
-          />
-          <div
-            hx-select="#username-result"
-            hx-swap="outerHTML"
-            id="username-result"
-          >
-            Username is taken
-          </div>
-        </div>
-      );
-    } else {
-      return (
-        <div>
-          <input hx-select="#set-username" id="set-username" value={username} />
-        </div>
-      );
-    }
-  },
-  {
-    body: t.Object({
-      username: t.String({ maxLength: 32 }),
-    }),
+const upsertUser = async (args: { username: string; id?: number }) => {
+  const { username, id } = args;
+  const result = await db
+    .insert(User)
+    .values({ username, id })
+    .onConflictDoUpdate({
+      // someone with this user id exists
+      // just update it with a new username (if it's not taken already)
+      target: User.id,
+      targetWhere: notExists(
+        db.select().from(User).where(eq(User.username, username))
+      ),
+
+      set: { username, createdAt: UNIX_EPOCH },
+
+      // if the creation time is older than a unix epoch, we would technically
+      // be replacing an existing user, I think
+    })
+    .onConflictDoNothing()
+    .returning({ id: User.id });
+
+  if (result.length === 0) {
+    return undefined;
+  } else if (result.length === 1) {
+    return result[0].id;
+  } else {
+    throw new Error("upsertUser updated more than one user!");
   }
-);
+};
+
+const selectCurrentUsersInGameSql = db
+  .select()
+  .from(User)
+  .where(
+    and(
+      or(eq(Game.playerX, User.id), eq(Game.playerO, User.id)),
+      lte(USER_AGE, DAY_LENGTH)
+    )
+  );
+
+const selectCurrentGameSql = (gameId: SQLiteColumn) =>
+  db
+    .select()
+    .from(Game)
+    .where(and(eq(gameId, Game.id), exists(selectCurrentUsersInGameSql)));
+
+const removeOutdatedActiveLobbiesSql = db
+  .delete(ActiveLobby)
+  .where(notExists(selectCurrentGameSql(ActiveLobby.gameId)))
+  .returning({ id: ActiveLobby.id })
+  .prepare();
+
+const removeOutdatedFinishedLobbiesSql = db
+  .delete(FinishedLobby)
+  .where(notExists(selectCurrentGameSql(FinishedLobby.gameId)))
+  .returning({ id: ActiveLobby.id })
+  .prepare();
+
+const removeLobbySql = db
+  .delete(Lobby)
+  .where(eq(Lobby.id, sql.placeholder("id")))
+  .prepare();
+
+const removeOutdatedUserSql = db
+  .delete(User)
+  .where(
+    and(
+      eq(User.id, sql.placeholder("id")),
+      gt(sql<number>`${UNIX_EPOCH} - ${User.createdAt}`, DAY_LENGTH)
+    )
+  )
+  .returning({ id: User.id })
+  .prepare();
+const removeOutdatedUser = async (args: { id: number }) => {
+  const { id } = args;
+  const result = await removeOutdatedUserSql.execute({ id });
+
+  if (result.length === 0) {
+    return undefined;
+  } else if (result.length === 1) {
+    return result[0].id;
+  } else {
+    throw new Error("removeOutdatedUser removed more than 1 user!");
+  }
+};
+
+const selectUsernameByIdSql = db
+  .select({ username: User.username })
+  .from(User)
+  .where(eq(User.id, sql.placeholder("id")))
+  .prepare();
+const selectUsernameById = async (args: { id: number }) => {
+  const result = await selectUsernameByIdSql.execute(args);
+  if (result.length === 0) {
+    return undefined;
+  } else if (result.length === 1) {
+    return result[0].username;
+  } else {
+    throw new Error("selected more than one user from an id!");
+  }
+};
+
+export default new Elysia({ prefix: "/api" })
+  .use(html())
+  .get(
+    "/username",
+    async ({ cookie: { userId: userIdCookie } }) => {
+      const userId = userIdCookie.value;
+      if (userId !== undefined) {
+        const username = await selectUsernameById({ id: userId });
+        if (username !== undefined) {
+          return { success: true, username };
+        }
+      }
+
+      return { success: false };
+    },
+    {
+      response: t.Union([
+        t.Object({
+          success: t.Literal(true),
+          username: t.String(),
+        }),
+        t.Object({ success: t.Literal(false) }),
+      ]),
+      cookie: t.Object({ userId: t.Optional(intString) }),
+    }
+  )
+  .put(
+    "/username",
+    async ({ body: { username }, cookie: { userId: userIdCookie } }) => {
+      let message: string | undefined = undefined;
+      if (username.length < 1) {
+        message = "username must have at least 1 character";
+      } else if (username.length > 32) {
+        message = "username must have at most 32 characters";
+      } else if (!ALPHANUMERIC.test(username)) {
+        message = "username must be entirely alphanumeric";
+      }
+
+      if (message !== undefined) {
+        return { success: false, message };
+      }
+
+      // get the person's stored user id
+      const userId: number | undefined = userIdCookie.value;
+
+      // if it doesn't exist, give them a new user id
+      // otherwise, update the existing one
+      // both possibilities are handled by upsertUser
+      const newUserId = await upsertUser({ username, id: userId });
+      if (newUserId === undefined) {
+        // a user id couldn't be added, so the username change failed
+        return { success: false, message: "username is taken" };
+      }
+
+      userIdCookie.value = newUserId;
+      userIdCookie.maxAge = DAY_LENGTH;
+
+      return { success: true };
+    },
+    {
+      body: t.Object({ username: t.String() }),
+      type: "application/x-www-form-urlencoded",
+      response: t.Union([
+        t.Object({ success: t.Literal(true) }),
+        t.Object({ success: t.Literal(false), message: t.String() }),
+      ]),
+      cookie: t.Object({ userId: t.Optional(intString) }),
+    }
+  );
