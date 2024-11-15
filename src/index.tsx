e1d7df44c2c2b712391923f7321cbd5eb46af9d3
goldenstein64 <goldnstein64@gmail.com>
2024-11-15 01:19:43 -0500

    chore: added swagger

--- src/index.tsx
+++ src/index.tsx
@@ -12,8 +12,10 @@ import gameApi from "./routes/game-api";
 import lobbyApi from "./routes/lobby-api";
 import userApi from "./routes/user-api";
 import { intString } from "./types";
+import swagger from "@elysiajs/swagger";
 
 const app = new Elysia({ name: "app" })
+  .use(swagger())
   .use(html())
   .use(JWTAuth)
   .get("/login", async ({ user }) => (user ? redirect("/", 302) : LoginHtml()))
