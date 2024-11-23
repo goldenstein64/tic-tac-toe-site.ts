cc11b589c46975a335a999b1f891eeca6196e3ab
goldenstein64 <goldnstein64@gmail.com>
2024-11-22 22:55:58 -0500

    chore: removed comment

--- src/routes/session-api.tsx
+++ src/routes/session-api.tsx
@@ -3,7 +3,6 @@ import { Elysia } from "elysia";
 export default new Elysia({ prefix: "/api" }).delete(
   "/session",
   async ({ cookie: { access: cookieAccess, refresh: cookieRefresh } }) => {
-    // just invalidate their cookies 👍
     cookieAccess.remove();
     cookieRefresh.remove();
   }
