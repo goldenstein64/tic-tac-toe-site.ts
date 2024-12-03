ea23169b3dfe962a542fd9b3563a33e02f87e422
goldenstein64 <goldnstein64@gmail.com>
2024-12-03 11:48:45 -0500

    chore: /debug/user reloads after request

--- src/components/debug.tsx
+++ src/components/debug.tsx
@@ -6,7 +6,12 @@ export function DebugPanel() {
   return (
     <div id="debug-panel">
       <h3>Debug Panel</h3>
-      <form class="get-user" hx-post="/debug/user" hx-swap="none">
+      <form
+        class="get-user"
+        hx-post="/debug/user"
+        hx-swap="none"
+        hx-on--after-request="location.reload()"
+      >
         <input type="text" name="usernameQuery" />
         <button type="submit">Get User</button>
       </form>
