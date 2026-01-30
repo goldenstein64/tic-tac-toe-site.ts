6906abdf8bba08148a14e3472b82df60851cde8f
goldenstein64 <goldnstein64@gmail.com>
2026-01-30 15:35:17 -0500

    test: put /login in a describe block

--- src/components/login.test.tsx
+++ src/components/login.test.tsx
@@ -1,10 +1,12 @@
-import { it, expect } from "bun:test";
+import { describe, it, expect } from "bun:test";
 import { Html } from "@elysiajs/html";
 
 import LoginHtml from "./login";
 import { setupDocument, getHTML } from "#/test/util";
 
-it("matches snapshot", async () => {
-  const document = await setupDocument(await (<LoginHtml />));
-  expect(await getHTML(document)).toMatchSnapshot();
+describe("GET /login", () => {
+  it("matches snapshot", async () => {
+    const document = await setupDocument(await (<LoginHtml />));
+    expect(await getHTML(document)).toMatchSnapshot();
+  });
 });
