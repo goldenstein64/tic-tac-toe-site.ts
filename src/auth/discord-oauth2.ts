d72d63d0a7979dcc481b9fa27a923a94db1391ce
goldenstein64 <goldnstein64@gmail.com>
2026-02-01 21:57:10 -0500

    refactor(discord-oauth2): group functions and imports more clearly

--- src/auth/discord-oauth2.ts
+++ src/auth/discord-oauth2.ts
@@ -1,14 +1,21 @@
-import * as arctic from "arctic";
+import type { RESTGetAPICurrentUserResult } from "discord-api-types/v10";
+import type { OAuth2Tokens } from "arctic";
+
+import {
+  Discord,
+  OAuth2RequestError,
+  generateState,
+  generateCodeVerifier,
+} from "arctic";
 import { ElysiaCookie } from "elysia/dist/cookies";
 import { Elysia } from "elysia";
-import type { RESTGetAPICurrentUserResult } from "discord-api-types/v10";
 
 const MINUTES = 60;
 
-const provider = new arctic.Discord(
+const provider = new Discord(
   Bun.env.DISCORD_CLIENT_ID,
   Bun.env.DISCORD_CLIENT_SECRET,
-  Bun.env.DISCORD_REDIRECT_URL
+  Bun.env.DISCORD_REDIRECT_URL,
 );
 
 const cookieDefaults: Partial<ElysiaCookie> = {
@@ -19,61 +26,60 @@ const cookieDefaults: Partial<ElysiaCookie> = {
   maxAge: 30 * MINUTES,
 };
 
-async function userInfo(
-  accessToken: string
-): Promise<RESTGetAPICurrentUserResult> {
-  const response = await fetch("https://discord.com/api/v10/users/@me", {
-    headers: {
-      Authorization: `Bearer ${accessToken}`,
-      Accept: "application/json",
-    },
-  });
-
-  return await response.json();
-}
-
 export default function discordOAuth2() {
   return new Elysia({ name: "arctic-discord-oauth2-wrapper" })
-    .error("OAUTH2_REQUEST_ERROR", arctic.OAuth2RequestError)
-    .derive({ as: "scoped" }, ({ cookie, query, redirect }) => {
-      return {
-        discord: {
-          userInfo,
-          createURL(scopes: string[]): URL {
-            const state = arctic.generateState();
-            cookie["state"].set({
-              value: state,
-              ...cookieDefaults,
-            });
-            const codeVerifier = arctic.generateCodeVerifier();
-            cookie["codeVerifier"].set({
-              value: codeVerifier,
-              ...cookieDefaults,
-            });
-            return provider.createAuthorizationURL(state, codeVerifier, scopes);
-          },
-          redirect(scopes: string[]): Response {
-            const url = this.createURL(scopes);
-            return redirect(url.href);
-          },
-          authorize(): Promise<arctic.OAuth2Tokens> {
-            if (cookie["state"].value !== query["state"])
-              throw Error("state mismatch");
+    .error("OAUTH2_REQUEST_ERROR", OAuth2RequestError)
+    .derive({ as: "scoped" }, ({ cookie, query, redirect }) => ({
+      discord: {
+        async userInfo(
+          accessToken: string,
+        ): Promise<RESTGetAPICurrentUserResult> {
+          const response = await fetch(
+            "https://discord.com/api/v10/users/@me",
+            {
+              headers: {
+                Authorization: `Bearer ${accessToken}`,
+                Accept: "application/json",
+              },
+            },
+          );
 
-            cookie["state"].remove();
-            const codeVerifier = cookie["codeVerifier"].value as string | null;
-            if (!codeVerifier)
-              throw new Error(
-                `Bug with ${String(provider)} and codeVerifier. Please open issue`
-              );
-
-            cookie["codeVerifier"].remove();
-            return provider.validateAuthorizationCode(
-              query["code"],
-              codeVerifier
-            );
-          },
+          return response.json();
         },
-      };
-    });
+        createURL(scopes: string[]): URL {
+          const state = generateState();
+          cookie["state"].set({
+            value: state,
+            ...cookieDefaults,
+          });
+          const codeVerifier = generateCodeVerifier();
+          cookie["codeVerifier"].set({
+            value: codeVerifier,
+            ...cookieDefaults,
+          });
+          return provider.createAuthorizationURL(state, codeVerifier, scopes);
+        },
+        redirect(scopes: string[]): Response {
+          const url = this.createURL(scopes);
+          return redirect(url.href);
+        },
+        authorize(): Promise<OAuth2Tokens> {
+          if (cookie["state"].value !== query["state"])
+            throw new Error("state mismatch");
+
+          cookie["state"].remove();
+          const codeVerifier = cookie["codeVerifier"].value as string | null;
+          if (!codeVerifier)
+            throw new Error(
+              `Bug with ${String(provider)} and codeVerifier. Please open issue`,
+            );
+
+          cookie["codeVerifier"].remove();
+          return provider.validateAuthorizationCode(
+            query["code"],
+            codeVerifier,
+          );
+        },
+      },
+    }));
 }
